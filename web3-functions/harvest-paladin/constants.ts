b6d1490a6bccfbbf7dc2485572dca0c8a1e00efc
Kapusniak <goldkapusniak@proton.me>
2023-11-22 21:32:30 +0300

    feat: update constant with latest deployment strategy address

--- web3-functions/harvest-paladin/constants.ts
+++ web3-functions/harvest-paladin/constants.ts
@@ -1,4 +1,4 @@
 export const PALADIN_MERKLE_CLAIM_URL = "https://claims.warden.vote/proof/delegation/"
 
-// TODO: replace with final strat deployment addr, use test addy for flow
-export const AURA_STRATEGY = "0x512fce9B07Ce64590849115EE6B32fd40eC0f5F3"
+// https://etherscan.io/address/0x0f57aE830120fBA149A4D922a9A0064de9755a5d#code
+export const AURA_STRATEGY = "0x0f57aE830120fBA149A4D922a9A0064de9755a5d"
